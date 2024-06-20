d5076cde120bba95db16c3a1c0303a9dd7af799f
priyanshu1019 <goutampriyanshu27@gmail.com>
2024-06-20 09:55:31 +0530

    added close button functionality

--- app/src/main/java/com/example/vit_assignment_2_birthday_card/MainActivity.kt
+++ app/src/main/java/com/example/vit_assignment_2_birthday_card/MainActivity.kt
@@ -1,20 +1,16 @@
-package com.example.vit_assignment_2_birthday_card
+package com.example.birthdaycardapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.vit_assignment_2_birthday_card.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+    }
+
+    fun closeApp(view: android.view.View) {
+        finish()
     }
 }
